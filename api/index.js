bcd3fb445fa01fdcb0eab520be725127cb70fbb6
mark <2632571622@qq.com>
2024-07-01 15:32:21 +0800

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Q3dIgYhMXHIzW5wg
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 // app.get()
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
