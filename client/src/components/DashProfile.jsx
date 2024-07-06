c52c63f344a60fb0b098fccdf0fec2f4793e454e
mark <2632571622@qq.com>
2024-07-06 18:39:19 +0800

    complete create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import {
   getDownloadURL,
@@ -22,7 +23,7 @@ import {
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -247,9 +248,23 @@ export default function DashProfile() {
           onChange={handleChange}
         />
 
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline disabled={ loading || imageFileUploading }>
+          { loading ? "Loading" : "Update" }
         </Button>
+
+        {
+          currentUser.user.isAdmin && (
+            <Link to="/create-post">
+              <Button
+                type="button"
+                gradientDuoTone="purpleToPink"
+                className="w-full"
+              >
+                Create a post
+              </Button>
+            </Link>
+          )
+        }
       </form>
 
       <div className="text-red-500 flex justify-between mt-5">
